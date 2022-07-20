cb62c1a5d3c4d7c8339320bbf2574eeb46bd918a
Lam-Laptop <lam.tran@aalto.fi>
2022-07-20 15:21:06 +0300

    2.8 - 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,44 +3,69 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
-  const addName = (event) => {
+  const add = (event) => {
     event.preventDefault()
 
     if (persons.every( p => p.name !== newName)) {
       const person = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(person))
       setNewName('')
+      setNewNumber('')
     }
     else alert(newName + ' is already added to phonebook')
     
   }
 
-  const handleChange = (event) => {
+  const handleNameInputChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumInputChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} onChange={handleFilterChange}/>
+      </div>
+
+      <h2>add new number</h2>
       <form>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleNameInputChange}/>
         </div>
         <div>
-          <button type="submit" onClick={addName}>
+          number: <input value={newNumber} onChange={handleNumInputChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={add}>
             add
           </button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
-        {persons.map( (p, i) => <Person person={p} key={i}/>)}
+        {persons.filter( p => p.name.toLowerCase().includes(newFilter.toLowerCase()))
+                .map( p => <Person person={p} key={p.id}/>)}
       </ul>
     </div>
   )
