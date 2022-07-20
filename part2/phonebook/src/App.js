f8be65215e0e5742fc7900022800b340da81a8de
Lam-Laptop <lam.tran@aalto.fi>
2022-07-20 13:30:41 +0300

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const person = {
+      name: newName
+    }
+    setPersons(persons.concat(person))
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addName}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map( (p, i) => <Person person={p} key={i}/>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,9 @@
+const Person = ({person}) => {
+    return(
+        <li>
+            {person.name}
+        </li>
+    )
+}
+
+export default Person
